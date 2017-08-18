c78126e6ddcf92bd32135372c92c8b26c52d6d0f
Ming <holy_yiming@qq.com>
2017-08-18 17:33:47 +0800

    exception handler

--- fx/src/main/java/pers/xym/fx/web/BaseAdminController.java
+++ fx/src/main/java/pers/xym/fx/web/BaseAdminController.java
@@ -1,5 +1,25 @@
 package pers.xym.fx.web;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import pers.xym.fx.util.ResponseJson;
+
+/**
+ * 后台基础Controller
+ * @author 徐一鸣
+ * @date 2017年8月18日
+ *
+ */
+@RestController
 public class BaseAdminController {
 
+	@ExceptionHandler
+	public ResponseJson handleException(Exception e) {
+		ResponseJson responseJson = new ResponseJson();
+		
+		if (e instanceof ) {
+			
+		}
+	}
 }
